f635ce79ede2b1906e907aaf5e273529d6723c77
Lucas Inocente <olucassanchez@gmail.com>
2020-12-19 00:15:32 -0300

    Carrega conteúdo dinâmico em página e gera html à partir de conteúdo markdown

--- helpers/getPage.js
+++ helpers/getPage.js
@@ -1,12 +1,18 @@
 
 import fs from 'fs'
 import matter from 'gray-matter'
+import marked from 'marked'
 
 const getPage = (path, folder = 'contents') => {
-  const file = `${process.cwd()}/${folder}/${path}.md`
-  const content = fs.readFileSync(file, 'utf8')
+  const filePath = `${process.cwd()}/${folder}/${path}.md`
+  const file = fs.readFileSync(filePath, 'utf8')
+  const { content: rawContent, data } = matter(file)
+  const content = marked(rawContent)
 
-  return matter(content)
+  return {
+    content,
+    data
+  }
 }
 
 export default getPage
