75f31825da78df7cc13f0ed8c65934935da2b1e0
Lucas Inocente <olucassanchez@gmail.com>
2020-12-18 23:56:01 -0300

    Adiciona parser para html retornado na API

--- pages/[section]/[id].js
+++ pages/[section]/[id].js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import ReactHtmlParser from 'react-html-parser'
 import Header from '../../components/Header'
 
 const Page = (
@@ -20,7 +21,7 @@ const Page = (
         <h1>{title}</h1>
         <div className="inner-container">
           <h2>{description}</h2>
-          <p>{ content }</p>
+          { ReactHtmlParser(content) }
         </div>
       </article>
     </>
@@ -32,7 +33,7 @@ Page.getInitialProps = async () => {
     data: {
       title: 'Comida, Não Bombas - Parte 3 - Estágio',
       description: 'Se você caiu aqui sem saber o que é um Comida Não Bombas, leia aqui primeiro:',
-      content: 'Saindo do campo das ideias e pesquisas, ontem tive a honra e o prazer de fazer uma pequena participação no grupo que distribui comida que a Maíra comentou (Veja a parte 2 no link acima).'
+      content: '<p>Asd</p><p>Saindo do campo das ideias e pesquisas, ontem tive a honra e o prazer de fazer uma pequena participação no grupo que distribui comida que a Maíra comentou (Veja a parte 2 no link acima).</p>'
     }
   }
 }
